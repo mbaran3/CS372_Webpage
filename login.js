90d37572e84fef639e1a1f7a2020a0639adc924a
Katherine Halliday <152108479+kathypillar@users.noreply.github.com>
2024-02-12 16:03:30 -0900

    Add files via upload
    added the old js from login.html

---
+++ login.js
@@ -0,0 +1,10 @@
+var p = "";
+function loginHdlr() {
+	document.getElementById("message").innerHTML = "Error: Everything is working as intended.";
+}
+function disguise() {
+
+	p=p+"*";
+	document.getElementById("pass").value = p;
+}
+
