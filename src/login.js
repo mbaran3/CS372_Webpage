df4e9ce0778dcc068911ea4a6eef4f0257026ed9
Katherine <kahalliday@alaska.edu>
2024-02-14 15:32:59 -0900

    As it turns out html can auto blot out your password

--- src/login.js
+++ src/login.js
@@ -1,10 +1,10 @@
-var p = "";
+//var p = "";
 function loginHdlr() {
 	document.getElementById("message").innerHTML = "Error: Everything is working as intended.";
 }
-function disguise() {
-
-	p=p+"*";
-	document.getElementById("pass").value = p;
-}
+//function disguise() {
+//
+//	p=p+"*";
+//	document.getElementById("pass").value = p;
+//}
 
