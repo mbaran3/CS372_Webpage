10a437ea1e154d85f88746aa981ba15d183804a4
Katherine <kahalliday@alaska.edu>
2024-02-16 15:31:12 -0900

    added some file templates

--- src/login.js
+++ src/login.js
@@ -1,10 +1,3 @@
-//var p = "";
 function loginHdlr() {
 	document.getElementById("message").innerHTML = "Error: Everything is working as intended.";
 }
-//function disguise() {
-//
-//	p=p+"*";
-//	document.getElementById("pass").value = p;
-//}
-
