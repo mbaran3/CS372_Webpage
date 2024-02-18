670459594b43d5940938d3c129874116ab0798bd
mbaran3 <mbaran3@alaska.edu>
2024-02-18 21:43:19 +0000

    basic back end handling of html pages

--- src/server.js
+++ src/server.js
@@ -1,1 +1,27 @@
 //todo literally everything
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const port = 8080;
+
+
+app.get('/', (req, res) =>{
+  res.sendFile(__dirname + '/login.html')
+});
+app.get('/signup.html', (req, res) =>{
+  res.sendFile(__dirname + '/signup.html')
+})
+
+app.listen(port, () =>{
+  console.log(`Server running on port : ${port}`)
+})
+
+
+app.post('/thePage.html', (req, res) =>{
+  res.sendFile(__dirname + '/thePage.html')
+})
+
+
